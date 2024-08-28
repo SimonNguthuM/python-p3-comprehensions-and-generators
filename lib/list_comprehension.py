ad24e06de0b0e36b7cfe14c34165cdad5ec8b6b5
Simon Nguthu <simon.nguthu@student.moringaschool.com>
2024-08-28 13:14:49 +0300

    List Comprehensions and Generator Expressions

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    list = [n for n in num_list if n % 2 == 0]
+    return list
 
 def make_exclamation(sentence_list):
-    pass
+    list = [string + "!" for string in sentence_list]
+    return list
